00c4799ddceab42b23ede01f434fc85f013a0e2e
macrespo42 <macrespo@student.42.fr>
2025-08-20 23:00:00 +0200

    feat: use gethostname

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,14 @@
+#include <netdb.h>
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("ping: usage error: Destination address required");
+    return 1;
+  }
+  struct hostent *host_entry;
+
+  host_entry = gethostbyname(argv[1]);
+  printf("Adress length: %s\n", host_entry->h_aliases[0]);
   return 0;
 }
