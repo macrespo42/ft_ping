56991e6030db76fc7719f117fcae37874adc3997
macrespo42 <macrespo@student.42.fr>
2025-08-21 11:46:25 +0200

    feat: print IP address

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,8 @@
+#include <arpa/inet.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <sys/socket.h>
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -9,6 +12,27 @@ int main(int argc, char **argv) {
   struct hostent *host_entry;
 
   host_entry = gethostbyname(argv[1]);
-  printf("Adress length: %s\n", host_entry->h_aliases[0]);
+
+  if (!host_entry) {
+    perror("gethostbyname");
+    return 1;
+  }
+
+  if (host_entry->h_addrtype != AF_INET) {
+    perror("Adress is not IPV4");
+    return 1;
+  }
+
+  struct in_addr **addr_list;
+
+  addr_list = (struct in_addr **)host_entry->h_addr_list;
+
+  if (addr_list[0] != NULL) {
+    // inet_ntoa convertit l'adresse binaire en chaîne de caractères
+    printf("Adresse IP: %s\n", inet_ntoa(*addr_list[0]));
+  } else {
+    printf("Aucune adresse IP trouvée pour cet hôte.\n");
+  }
+
   return 0;
 }
